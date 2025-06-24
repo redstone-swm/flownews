1781340535afa74b56e25b378e096394440efaa2
sukkyun2 <goobghd@gmail.com>
2025-06-24 15:38:16 +0900

    chore: cors allow-origins 프로퍼티 설정으로 분리
    - 기본 localhost:3000 적용
    - ALLOWED_ORIGINS 환경변수를 통해 제어하도록 변경

--- src/main/kotlin/com/flownews/config/WebConfig.kt
+++ src/main/kotlin/com/flownews/config/WebConfig.kt
@@ -1,14 +1,17 @@
 package com.flownews.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    @Value("\${spring.cors.allowed-origins}") private val allowedOrigins: String
+) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // 프론트엔드 주소
+            .allowedOrigins(*allowedOrigins.split(",").toTypedArray()) // 프론트엔드 주소
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
