f831efb0fef103afe9aa836a854cea2975f0d590
pgh <pggggggggh@yonsei.ac.kr>
2025-07-08 01:54:23 +0900

    refactor: 메소드 이름 수정

--- src/main/kotlin/com/flownews/api/user/api/UserMeQueryApi.kt
+++ src/main/kotlin/com/flownews/api/user/api/UserMeQueryApi.kt
@@ -1,25 +1,17 @@
 package com.flownews.api.user.api
 
 import com.flownews.api.user.app.CustomOAuth2User
-import com.flownews.api.user.domain.UserRepository
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SecurityRequirement(name = "bearerAuth")
 @RestController
-class UserMeQueryApi @Autowired constructor(
-    private val userRepository: UserRepository
-) {
+class UserMeQueryApi {
     @GetMapping("/users/me")
-    fun me(@AuthenticationPrincipal principal: CustomOAuth2User?): ResponseEntity<Any> {
-        if (principal == null) {
-            return ResponseEntity.status(401).body("Unauthorized")
-        }
+    fun getCurrentUser(@AuthenticationPrincipal principal: CustomOAuth2User): ResponseEntity<Any> {
         val user = principal.getUser()
         val response = mapOf(
             "id" to user.id,
