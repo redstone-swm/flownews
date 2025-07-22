08396bc30480017959ff57dc9d4049b581cfa47f
sukkyun2 <goobghd@gmail.com>
2025-07-22 14:25:09 +0900

    fix: 사용자 이벤트 로그 IP 주소 오류
    - 스와이프 이벤트시, IP 주소에 상관없이 127.0.0.1로 저장되는 이슈 수정
    - 클라이언트에서 값 받지않고 서버에서 가져오는 것으로 변경

--- src/main/kotlin/com/flownews/api/topic/api/TopicHistoryRecordApi.kt
+++ src/main/kotlin/com/flownews/api/topic/api/TopicHistoryRecordApi.kt
@@ -3,6 +3,7 @@ package com.flownews.api.topic.api
 import com.flownews.api.topic.app.TopicHistoryRecordRequest
 import com.flownews.api.topic.app.TopicHistoryRecordService
 import com.flownews.api.user.app.CustomOAuth2User
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,10 +18,11 @@ class TopicHistoryRecordApi(private val topicHistoryRecordService: TopicHistoryR
     fun recordTopicHistory(
         @PathVariable topicId: Long,
         @AuthenticationPrincipal user: CustomOAuth2User?,
-        @RequestBody req: TopicHistoryRecordRequest
+        @RequestBody req: TopicHistoryRecordRequest,
+        request: HttpServletRequest
     ): ResponseEntity<Void> {
 
-        topicHistoryRecordService.recordHistory(req.withTopic(topicId), user)
+        topicHistoryRecordService.recordHistory(req.with(topicId, request.remoteAddr), user)
 
         return ResponseEntity.ok().build()
     }
