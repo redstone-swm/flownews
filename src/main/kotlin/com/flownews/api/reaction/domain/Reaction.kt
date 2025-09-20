22d33262ce46fe846f22c96acf49d3086728676f
pgh <pggggggggh@yonsei.ac.kr>
2025-09-21 03:47:47 +0900

    fix: add id field to Reaction model

--- src/main/kotlin/com/flownews/api/reaction/domain/Reaction.kt
+++ src/main/kotlin/com/flownews/api/reaction/domain/Reaction.kt
@@ -6,6 +6,9 @@ import com.flownews.api.user.domain.User
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
@@ -14,6 +17,9 @@ import java.time.LocalDateTime
 @Entity
 @Table(name = "reactions")
 class Reaction(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     val user: User,
