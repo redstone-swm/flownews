c3b929f7bbda5d52690eb0e97170ce012dea8799
sukkyun2 <goobghd@gmail.com>
2025-10-03 11:56:18 +0900

    chore: remove unused code

--- src/main/kotlin/com/flownews/api/topic/app/TopicQueryService.kt
+++ src/main/kotlin/com/flownews/api/topic/app/TopicQueryService.kt
@@ -1,7 +1,6 @@
 package com.flownews.api.topic.app
 
 import com.flownews.api.common.app.NoDataException
-import com.flownews.api.reaction.domain.ReactionRepository
 import com.flownews.api.topic.domain.TopicRepository
 import com.flownews.api.topic.domain.TopicSubscriptionRepository
 import org.springframework.stereotype.Service
@@ -10,32 +9,7 @@ import org.springframework.stereotype.Service
 class TopicQueryService(
     private val topicRepository: TopicRepository,
     private val topicSubscriptionRepository: TopicSubscriptionRepository,
-    private val reactionRepository: ReactionRepository,
 ) {
-//    fun getTopic(
-//        id: Long,
-//        user: CustomOAuth2User?,
-//    ): TopicDetailsResponse {
-//        val topic = topicRepository.findById(id).orElseThrow { NoDataException("topic not found : $id") }
-//        val randomTopics = getRandomTopics(id)
-//
-//        if (user == null) {
-//            val recommendTopics = randomTopics.map(TopicSummaryResponse::fromEntity)
-//            return TopicDetailsResponse.fromEntity(topic, recommendTopics, reactionRepository)
-//        }
-//
-//        val userId = user.getUser().requireId()
-//        val topicHistory = topicHistoryRepository.findByTopicIdAndUserId(id, userId)
-//        val isFollowing = topicSubscriptionRepository.existsByTopicIdAndUserId(id, userId)
-//
-//        val recommendTopics = randomTopics.map { recommendTopic ->
-//            val isRecommendFollowing = topicSubscriptionRepository.existsByTopicIdAndUserId(recommendTopic.requireId(), userId)
-//            TopicSummaryResponse.fromEntity(recommendTopic, isRecommendFollowing)
-//        }
-//
-//        return TopicDetailsResponse.fromEntity(topic, recommendTopics, topicHistory, reactionRepository, isFollowing)
-//    }
-
     fun getTopicWithSubscribers(id: Long): TopicWithSubscribers {
         val topic = topicRepository.findById(id).orElseThrow { NoDataException("topic not found : $id") }
         val subscriptions = topicSubscriptionRepository.findByTopicId(id)
