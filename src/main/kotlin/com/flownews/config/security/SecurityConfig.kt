6d081b3c6057de506c80ad315a6717628683b44d
pgh <pggggggggh@yonsei.ac.kr>
2025-10-15 17:11:30 +0900

    feat: allow register without gender and birthdate

--- src/main/kotlin/com/flownews/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/flownews/config/security/SecurityConfig.kt
@@ -33,7 +33,6 @@ class SecurityConfig(
                 it.requestMatchers("/events/*").permitAll()
                 it.requestMatchers("/events/feed").permitAll()
                 it.requestMatchers("/notifications/push").permitAll()
-                it.requestMatchers("/v3/**").permitAll()
                 it.anyRequest().authenticated()
             }.oauth2Login {
                 it.successHandler { _, response, authentication ->
